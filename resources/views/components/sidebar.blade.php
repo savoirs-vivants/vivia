c6766091d09b64634c7dd550c37efadf9606d3f1
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-04-02 12:35:41 +0200

    Gestions des rôles pouvant voir certains éléments de la sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -35,43 +35,43 @@
                     'route'  => 'dashboard',
                     'label'  => 'Dashboard',
                     'icon'   => 'M4 5a1 1 0 011-1h4a1 1 0 011 1v5a1 1 0 01-1 1H5a1 1 0 01-1-1V5zm10 0a1 1 0 011-1h4a1 1 0 011 1v2a1 1 0 01-1 1h-4a1 1 0 01-1-1V5zM4 15a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1H5a1 1 0 01-1-1v-4zm10-3a1 1 0 011-1h4a1 1 0 011 1v7a1 1 0 01-1 1h-4a1 1 0 01-1-1v-7z',
-                    'role'   => null,
+                    'roles'  => null,
                 ],
                 [
                     'route'  => 'adherents.index',
                     'label'  => 'Adhérents',
                     'icon'   => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4',
-                    'role'   => null,
+                    'roles'  => null,
                 ],
                 [
                     'route'  => 'activites.index',
                     'label'  => 'Activités & Stages',
                     'icon'   => 'M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4',
-                    'role'   => null,
+                    'roles'  => null,
                 ],
                 [
                     'route'  => 'ressourcerie.index',
                     'label'  => 'Ressourcerie',
                     'icon'   => 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4',
-                    'role'   => null,
+                    'roles'  => null,
                 ],
                 [
                     'route'  => 'statistiques.index',
                     'label'  => 'Statistiques',
                     'icon'   => 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
-                    'role'   => null,
+                    'roles'  => ['admin', 'comptable'],
                 ],
                 [
                     'route'  => 'backoffice',
                     'label'  => 'Équipe',
                     'icon'   => 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
-                    'role'   => 'not_lecteur',
+                    'roles'  => ['admin'],
                 ],
             ];
         @endphp
 
         @foreach ($navItems as $item)
-            @if ($item['role'] === 'not_lecteur' && Auth::user()->role === 'lecteur')
+            @if (!empty($item['roles']) && !in_array(Auth::user()->role, $item['roles']))
                 @continue
             @endif
 
